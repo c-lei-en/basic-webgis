f229165171f1f1a1ceef3c9aa309135c76d6531e
纯洁 <1762928628@qq.com>
2020-03-30 07:58:55 +0800

    增加注释

--- src/components/basicLayer.vue
+++ src/components/basicLayer.vue
@@ -98,7 +98,7 @@ export default {
       this.googleMap.setVisible(!flag);
       this.aMap.setVisible(flag);
     },
-    //创建
+    //创建矢量化图层
     createVector: function() {
       this.vectorSou = new vectorSource();
       this.vectorLay = new vectorLayer({
@@ -119,6 +119,7 @@ export default {
       });
       this.map.addLayer(this.vectorLay);
     },
+    // 获取长度或面积
     getGeomLengthOrArea: function(geomType) {
       if (this.vectorLay == null) {
         this.createVector();
@@ -127,6 +128,7 @@ export default {
       this.addInteraction(geomType);
       this.map.addInteraction(this.draw);
     },
+    // 将测量工具添加到map中
     addInteraction: function(geomType) {
       let type = geomType == "Polygon" ? "Polygon" : "LineString";
       this.draw = new Draw({
@@ -153,9 +155,11 @@ export default {
         })
       });
       let output, coordinate;
+      // 开始绘画事件
       this.draw.on("drawstart", ev => {
         this.sketch = ev.feature;
         coordinate = ev.coordinate;
+        // 实时监测
         this.listener = this.sketch.getGeometry().on("change", ev => {
           let geom = ev.target;
           if (geom instanceof Polygon) {
@@ -169,6 +173,7 @@ export default {
           this.measureTooltip.setPosition(coordinate);
         });
       });
+      // 绘画结束
       this.draw.on("drawend", ev => {
         this.sketch = null;
         this.measureTooltipElement.push({ value: output });
@@ -177,6 +182,7 @@ export default {
         });
         let divId = "step" + index;
         let element;
+        // 当div的id动态加载好之后设置覆盖层以及要素的id方便后面进行删除
         this.$nextTick(() => {
           element = document.getElementById(divId);
           this.measureTooltip.setElement(element);
@@ -188,6 +194,7 @@ export default {
         this.map.removeInteraction(this.draw);
       });
     },
+    // 计算长度并且转换单位
     formatLength: function(line) {
       let length = getLength(line);
       let output;
@@ -198,6 +205,7 @@ export default {
       }
       return output;
     },
+    // 计算面积并且换算单位
     formatArea: function(polygon) {
       let area = getArea(polygon);
       let output;
@@ -208,6 +216,7 @@ export default {
       }
       return output;
     },
+    // 创建覆盖层显示长度或面积
     createMeasureTooltip: function() {
       if (this.measureMoveTooltip) {
         this.measureMoveTooltip = null;
@@ -221,6 +230,7 @@ export default {
       });
       this.map.addOverlay(this.measureTooltip);
     },
+    // 删除覆盖层以及当前feature
     deleteMeasure: function(index) {
       let featureId = this.step(index);
       let feature = this.vectorSou.getFeatureById(featureId);
@@ -231,6 +241,7 @@ export default {
       this.map.removeOverlay(overlay);
       this.measureTooltipElement[index] = null;
     },
+    // 得到当前div的下标
     getArrayIndex: function(arr, obj) {
       var i = arr.length;
       while (i--) {
@@ -240,6 +251,7 @@ export default {
       }
       return -1;
     },
+    // 动态设置id
     step: function(i) {
       return "step" + i;
     }
